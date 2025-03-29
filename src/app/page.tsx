5d1b94724ddd0ad6006f821266130d7de4afec24
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-03-30 00:09:42 +0530

    added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
-import LogoutButton from "@/components/Logout-Button";
+import { FeaturesSection } from "@/components/features-section";
+import { HeroSection } from "@/components/hero-section";
+import { HowItWorks } from "@/components/how-it-works";
+import { CtaSection } from "@/components/cta-section"
+import { Navbar } from "@/components/navbar";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import { Footer } from "@/components/footer";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
   if(!session){
-    return(
-      <div>
-        Loadfing....
-      </div>
-    )
+    redirect('/signin')
   }
 
   return (
-    <div>
-      <LogoutButton session={session}/>
+    <div className="relative min-h-screen">
+      <Navbar />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorks />
+        <CtaSection />
+      </main>
+      <Footer />
     </div>
   );
 }
